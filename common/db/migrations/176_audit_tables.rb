0d3047c9c2e8a936d6d30c5eff24534890447230
James Bullen <jambun@gmail.com>
2026-04-22 13:12:16 +1000

    getting serious about the standard

--- common/db/migrations/176_audit_tables.rb
+++ common/db/migrations/176_audit_tables.rb
@@ -4,23 +4,14 @@ Sequel.migration do
       primary_key :id
       String :uuid, :null => false
       DateTime :timestamp, :null => false
-      String :actor, :null => false
-      String :source_repo_uri
-      String :target_repo_uri
+      String :actor_name, :null => false
+      String :actor_type
+      String :object_uri, :null => false
+      String :object_type, :null => false
+      String :origin_uri
+      String :target_uri
       String :activity_type, :null => false
       String :change_method, :null => false
-    end
-
-    create_table(:audit_record) do
-      primary_key :id
-      Integer :audit_event_id, :null => false
-      String :record_type, :null => false
-      String :source_uri
-      String :target_uri, :null => false
-    end
-
-    alter_table(:audit_record) do
-      add_foreign_key([:audit_event_id], :audit_event, :key => :id)
     end
   end
 end
