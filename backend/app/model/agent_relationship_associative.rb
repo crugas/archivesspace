3de524caf65aae495df40edf810620bb8a82051b
Payten Giles <payten.giles@gmail.com>
2026-08-13 14:24:38 +1000

    The audit event system requires all models that refer to a enumeartion to set their scope

--- backend/app/model/agent_relationship_associative.rb
+++ backend/app/model/agent_relationship_associative.rb
@@ -3,4 +3,5 @@ class AgentRelationshipAssociative < Sequel::Model(:related_agents_rlshp)
   include ASModel
   corresponds_to JSONModel(:agent_relationship_associative)
 
+  set_model_scope :global
 end
